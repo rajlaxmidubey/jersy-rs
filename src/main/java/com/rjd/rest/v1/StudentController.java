87b765598052f9111a498fa7e74954c45370f358
Rajlaxmi Dubey <rajlaxmi.dubey01@gmail.com>
2019-08-10 13:18:51 +0530

    Javax Rest service example using jersy

---
+++ src/main/java/com/rjd/rest/v1/StudentController.java
@@ -0,0 +1,21 @@
+package com.rjd.rest.v1;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+@Path(value = "/Student")
+public class StudentController {
+	
+	@GET
+	@Path(value = "/{name}/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	 public  Student getStudent(@PathParam("name") String name, @PathParam("id")int id ) {
+		Student st = new Student ();
+		st.setId(id);
+		st.setName(name);
+		return st;
+	}
+
+}
